8b340bf6a32bb2e40a46f5ce80598466ac473c37
Aleksey Bobylev <al.bobylev@gmail.com>
2019-12-04 13:19:36 +0200

    Show printable links targets in footnotes (pdf)

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -217,7 +217,7 @@ latex_logo = '_static/logo.png'
 
 # If true, show URL addresses after external links.
 #latex_show_urls = False
-latex_show_urls = True
+latex_show_urls = 'footnote'
 
 # Documents to append as an appendix to all manuals.
 #latex_appendices = []
